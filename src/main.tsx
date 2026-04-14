10471e8f3f74cff38bbfb8c1eddd39840d26cb93
darunbjork <darunbjork@gmail.com>
2026-04-14 21:24:12 +0200

    feat: Integrate ToastProvider globally in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { AuthProvider } from './context/AuthContext.tsx';
-import { HouseholdProvider } from './context/HouseholdContext.tsx';
-import { DeviceProvider } from './context/DeviceContext.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { AuthProvider } from './context/AuthContext.tsx'
+import { HouseholdProvider } from './context/HouseholdContext.tsx'
+import { DeviceProvider } from './context/DeviceContext.tsx';
+import { ToastProvider } from './context/ToastContext.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <AuthProvider>
-      <HouseholdProvider>
-        <DeviceProvider> 
-          <App />
-        </DeviceProvider>
-      </HouseholdProvider>
+      <ToastProvider>
+        <HouseholdProvider>
+          <DeviceProvider>
+            <App />
+          </DeviceProvider>
+        </HouseholdProvider>
+      </ToastProvider>
     </AuthProvider>
-  </StrictMode>,
-);
+  </React.StrictMode>,
+)
